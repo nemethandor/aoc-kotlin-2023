e2e0df6744577b1c0694aca125dd3a897c5839f5
nemethandor <n.andor13@gmail.com>
2023-12-02 16:00:02 +0100

    Day02: part 2

--- src/Day02.kt
+++ src/Day02.kt
@@ -2,6 +2,16 @@ fun main() {
 
     val numberOfCubes = mapOf("red" to 12, "green" to 13, "blue" to 14)
 
+    fun getCubesOfSet(set: String): Map<String, Int> {
+        val cubes = buildMap {
+            set.split(",").map {
+                val cube = it.trim().split(" ")
+                put(cube[1], cube[0].toInt())
+            }
+        }
+        return cubes
+    }
+
     fun part1(input: List<String>): Int {
         return input.sumOf { line ->
             val game = line.split(":")
@@ -9,12 +19,7 @@ fun main() {
             var possibleSet = true
             var index = 0
             while (possibleSet && index < sets.size) {
-                val cubes = buildMap {
-                    sets[index].split(",").map {
-                        val cube = it.trim().split(" ")
-                        put(cube[1], cube[0].toInt())
-                    }
-                }
+                val cubes = getCubesOfSet(sets[index])
                 cubes.map { (key, value) ->
                     if (value > (numberOfCubes[key] ?: 0)) {
                         possibleSet = false
@@ -31,12 +36,25 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.sumOf { line ->
+            val game = line.split(":")
+            val sets = game[1].split(";")
+            val minimumNumberOfCubes = buildMap {
+                numberOfCubes.map { (color, _) ->
+                    val maximumNumberOfCube = sets.map { set -> getCubesOfSet(set) }.mapNotNull { it[color] }.maxOrNull() ?: 0
+                    if (maximumNumberOfCube != 0) {
+                        put(color, maximumNumberOfCube)
+                    }
+                }
+            }
+            minimumNumberOfCubes.values.reduce { accumulator, element -> accumulator * element }
+        }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day02_test")
     check(part1(testInput) == 8)
+    check(part2(testInput) == 2286)
 
     val input = readInput("Day02")
     part1(input).println()
